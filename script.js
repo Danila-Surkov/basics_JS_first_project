aa8846e122a7b4920cd8afc68542e0533fff6f3a
Danila <surkov.danila2001@yandex.ru>
2023-03-27 14:27:10 +0400

    add practic  4

--- script.js
+++ script.js
@@ -1,69 +1,81 @@
 'use strict'
-let numberOfFilms;
 
-function start() {
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
-    
-    while (numberOfFilms == "" || numberOfFilms.length > 50 || numberOfFilms == null || isNaN(numberOfFilms)){
-        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
-    }
-}
-
-start();
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start:  function(){
+        personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", '');
+        while (personalMovieDB.count == "" || personalMovieDB.count.length > 50 || personalMovieDB.count == null || isNaN(personalMovieDB.count)){
+            personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", '');
+        }
+    },
+
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            const titleLastMovie = prompt("Один из последних просмотренных фильмов?", ''),
+                 gradeLastMovie = prompt("На сколько оцените его?", '');
+             if (titleLastMovie != null && gradeLastMovie != null && titleLastMovie != '' && gradeLastMovie != '' && titleLastMovie.length < 50 && gradeLastMovie.length < 50 && titleLastMovie != ' ' && gradeLastMovie != ' '){
+                 personalMovieDB.movies[titleLastMovie] = gradeLastMovie;
+                 console.log("good");
+             } else {
+                 console.log("BAD");
+                 i--;
+             }  
+        }
+    },
+
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10){
+            alert("Просмотрено довольно мало фильмов");
+        }  else if (personalMovieDB.count <= 30){
+            alert("Вы классический зритель");
+        } else if (personalMovieDB.count > 30) {
+            alert("Вы киноман");
+        } else {
+            alert("Произошла ошибка");
+        }
+    },
+
+    showMyDB: function(hidden){
+        if (!hidden) {
+            console.log(personalMovieDB);
+        } 
+    },
+
+    writeYourGenres: function() {
+        
+        for (let i = 1, y = 0; i <= 3; i++, y++) {
+            while (personalMovieDB.genres[y] == ""  || personalMovieDB.genres[y] == null ){
+                personalMovieDB.genres[y] = prompt(`Ваш любимый жанр под номером ${i}`, '');
+            } 
+        }
+        
+        personalMovieDB.genres.forEach((element, i) => {
+           console.log(`Любимый жанр #${i + 1} - это ${element}`); 
+        });
+
+    },
+
+    toggleVisibleMyDB: function() {
+        if (personalMovieDB.privat){
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
 };
+personalMovieDB.start();
+personalMovieDB.rememberMyFilms();
+personalMovieDB.detectPersonalLevel();
+personalMovieDB.showMyDB(personalMovieDB.privat);
+personalMovieDB.writeYourGenres();
+personalMovieDB.toggleVisibleMyDB();
 
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const titleLastMovie = prompt("Один из последних просмотренных фильмов?", ''),
-             gradeLastMovie = prompt("На сколько оцените его?", '');
-         if (titleLastMovie != null && gradeLastMovie != null && titleLastMovie != '' && gradeLastMovie != '' && titleLastMovie.length < 50 && gradeLastMovie.length < 50 && titleLastMovie != ' ' && gradeLastMovie != ' '){
-             personalMovieDB.movies[titleLastMovie] = gradeLastMovie;
-             console.log("good");
-         } else {
-             console.log("BAD");
-             i--;
-         }  
-     }
-}
 
-rememberMyFilms();
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10){
-        alert("Просмотрено довольно мало фильмов");
-    }  else if (personalMovieDB.count <= 30){
-        alert("Вы классический зритель");
-    } else if (personalMovieDB.count > 30) {
-        alert("Вы киноман");
-    } else {
-        alert("Произошла ошибка");
-    }
-}
-
-detectPersonalLevel();
-
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-
-showMyDB(personalMovieDB.privat);
-
-function writeYourGenres() {
-    for (let i = 1, y = 0; i <= 3; i++, y++) {
-        personalMovieDB.genres[y] = prompt(`Ваш любимый жанр под номером ${i}`, ''); 
-    }
-}
-
-writeYourGenres()
 
 /*
 let count = 0;
--- task.js
+++ task.js
@@ -100,6 +100,7 @@ const arrayOfNumbers = [];
     }
     console.log(result);
 
+    NASTYA 
     function getMathResult(baseNum, repeatNum) {
         if(repeatNum > 0 && typeof(repeatNum) == "number"){
             let resStr = "";
@@ -130,7 +131,7 @@ console.log(returnNeighboringNumbers(5));
 function sayHello(name) {
     return `Привет, ${name}!`;
 } 
-
+END NASTYA
 
 function returnNeighboringNumbers(number) {
 
@@ -155,3 +156,298 @@ function getMathResult(number, quantity) {
 
 console.log(getMathResult(5,3));
 */
+/**DANILA */
+function calculateVolumeAndArea(number) {
+    if (number !== '' && number > 0 && Number.isInteger(number)) {
+        let V = number * number * number;
+        let S = 6 * number * number;
+        return `Объем куба: ${V}, площадь всей поверхности: ${S}`;
+    } else {
+        return "При вычислении произошла ошибка";}
+}
+
+// Место для второй задачи
+function getCoupeNumber(place) {
+    if (place > 0 && place < 37 && Number.isInteger(place) && place !== "") {
+        let carriage = Math.ceil(place / 4);
+        return carriage;
+    } else if (place == 0 || place > 36) { 
+        return "Таких мест в вагоне не существует";}
+    else{
+        return "Ошибка. Проверьте правильность введенного номера места";
+    }
+}
+
+/*NASTYA
+// Место для первой задачи
+function calculateVolumeAndArea(ribLength) {
+    if (Number.isInteger(ribLength) && ribLength > 0 && typeof (ribLength) == 'number'){
+        let volume = ribLength*ribLength*ribLength;
+        let area = ribLength*ribLength*6;
+        return `Объем куба: ${volume}, площадь всей поверхности: ${area}`;
+    } else {
+        return "При вычислении произошла ошибка";
+    }
+}
+
+// Место для второй задачи
+function getCoupeNumber(place) {
+    if (typeof (place) !== 'number' || place < 0 || !Number.isInteger(place)){
+        return "Ошибка. Проверьте правильность введенного номера места";
+    } else if (place === 0 || place > 36) {
+        return "Таких мест в вагоне не существует";
+    } else {
+        return Math.ceil(place/4);
+    }
+}
+
+*/
+
+/**  DANILA
+function findMaxNumber(a,b,c,d) {
+    if (a !== '' && a >= 0 && b !== '' && b >= 0 && c !== '' && c >= 0 && d !== '' && d >= 0 && typeof(a) == "number" && typeof(b) == "number" && typeof(c) == "number" && typeof(d) == "number") {
+        let max = a;
+        if (max < b){
+            max = b;
+        } 
+        if (max < c){
+            max = c;
+        } 
+        if (max < d){
+            max = d;
+        }
+        return max;
+    } else {
+        return 0;
+    }
+}
+
+console.log(findMaxNumber(1,5,6.6,11));
+
+
+
+function fib(number) {
+    let a = [0,1];
+    if (number > 1 && typeof(number) == "number" && number !== '' && Number.isInteger(number)) {
+        for (let i = 2 ; i < number; i++){
+            a[i] = a[i-2] + a[i-1];
+        } return a.join(" ");
+    } else if (number == 1){
+        return "0";
+    } else {
+        return "";
+    }
+}
+*/
+/** NASTYA
+function findMaxNumber(a, b, c, d) {
+    if (typeof(a) != 'number' || typeof(b) != 'number' || typeof(c) != 'number' || typeof(d) != 'number'){
+        return `${a} , ${b} , ${c} , ${d}`;
+    }
+}
+console.log(findMaxNumber(1,5,4));
+
+function fib(num) {
+    if (typeof(num) != 'number' || !Number.isInteger(num) || num <= 0){
+        return "";
+    } else {
+        if (num == 1){
+            return '0';
+        } else {
+            let arr = [0, 1];
+            for (let i = 1; i < num; i++) {
+                arr[i+1] = arr[i-1] + arr[i];
+            }
+            return arr.join(" ");
+        }
+    }
+}
+
+console.log(fib(7));*/
+/** NASTYA
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
+        },
+        exp: '1 month'
+    },
+    showAgeAndLangs: function(obj){
+        const {age} = obj;
+        const {languages} = obj.skills;
+        let str = `Мне ${age} и я владею языками: ` ;
+        for (let key in languages){
+            str+=`${languages[key].toUpperCase()} `;
+        }
+        return str;
+    }
+};
+
+function showExperience(plan) {
+    const {exp} = plan.skills
+    return exp;
+}
+
+console.log(showExperience(personalPlanPeter));
+ 
+
+function showProgrammingLangs(plan) {
+    let result = "";
+    const {programmingLangs} = plan.skills;
+    for (let key in programmingLangs){
+        result+=`Язык ${key} изучен на ${programmingLangs[key]}\n`;
+    }
+    return result;
+}
+
+console.log(showProgrammingLangs(personalPlanPeter));
+console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
+*/
+
+/** DANILA
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
+        },
+        exp: '1 month'
+    },
+    showAgeAndLangs: function(plan) {
+        const {age} = plan;
+        const {languages} = plan.skills;
+        const last = languages.join(" ");
+        const lang = last.toUpperCase();
+        return `Мне ${age} и я владею языками: ${lang}`;
+        
+    }
+};
+
+console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
+
+const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+
+function standardizeStrings(arr) {
+    let arrChange = [];
+    for (let i = 0; i < arr.length  ; i++) {
+        arrChange[i] = arr[i].toLowerCase();
+        console.log(arrChange[i])
+    }
+}
+
+console.log(standardizeStrings(favoriteCities));
+
+const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+
+function standardizeStrings(arr) {
+    let string = arr.join();
+    let words = string.toLowerCase()
+    for (let i = 0; i < arr.length ; i++) {
+        words = words.replace("," , "\n");
+    } return words;
+}
+
+
+const baseCurrencies = ['USD', 'EUR','UAH', 'RUB', 'CNY'];
+const additionalCurrencies = ['USD'];
+
+function availableCurr(arr, missingCurr) {
+    let str = 'Нет доступных валют';
+    if ( arr.length !== 0) {
+        str ='Доступные валюты:\n';
+        for(let key of arr){
+            if(key!= missingCurr){
+                str += `${key}\n`;
+            }
+        } return str } else {
+        return str;
+    }     
+}
+
+console.log(availableCurr(baseCurrencies, additionalCurrencies))
+
+const baseCurrencies = ['USD', 'EUR'];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+
+function availableCurr(arr, missingCurr) {
+    if (arr.length !== 0){
+        console.log(missingCurr);
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] == missingCurr){
+                delete arr[i];
+                arr[i] = arr[i+1];
+            } 
+        } 
+        let words = arr.join();
+        for (let y = 0; y < arr.length; y++){
+            words = words.replace("," , "\n");
+        }
+        if (typeof(missingCurr) == "undefined"){
+        return "Доступные валюты:" + "\n" + words + "\n"
+        } else {
+            return "Доступные валюты:" + "\n" + words
+        }
+    } else { 
+        return "Нет доступных валют"
+    }
+}
+
+console.log(availableCurr(baseCurrencies, additionalCurrencies))
+*/
+
+/**NASTYA 
+const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+
+function showFamily(arr) {
+    let res ="Семья состоит из: ";
+    if (arr.length === 0){
+        return 'Семья пуста';
+    } else {
+        for(let key of arr){
+            res+=`${key} `;
+        }
+        return res;
+    }
+}
+console.log(showFamily(family));
+
+const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+
+function standardizeStrings(arr) {
+    for(let i = 0; i < arr.length; i++){
+        console.log(arr[i].toLowerCase());
+    }
+}
+
+standardizeStrings(favoriteCities);
+
+const someString = 'This is some strange string';
+
+function reverse(str) {
+    const arr = Array.from(str);
+    return arr.reverse().join('');
+}
+console.log(reverse(someString));
+
+const baseCurrencies = ['USD', 'EUR'];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+
+function availableCurr(arr, missingCurr) {
+    let resStr = '';
+    arr.length === 0 ? resStr = 'Нет доступных валют': resStr = 'Доступные валюты:\n'
+    const result = arr.filter(a => a != missingCurr);
+    for (let key of result){
+        resStr += `${key}\n`
+    } 
+    return resStr;
+}
+
+console.log(availableCurr(['UAH', 'RUB', 'CNY'], 'CNY'));*/
+
